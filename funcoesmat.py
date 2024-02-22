5f8a45d38997b8e3bf6a7cdb31b69cdb754998f2
Diego Rodolfo Inacio <rodolfo.diego@yahoo.com.br>
2024-02-22 16:29:54 -0300

    Criando arquivo de funções matemáticas

---
+++ funcoesmat.py
@@ -0,0 +1,15 @@
+# Retorna valor absoluto #
+print (abs(-3.5))
+
+# Retorna o minimo de um conjunto #
+print (min(7,19,15,8,3))
+
+# Retorna o máximo de um conjunto #
+print (max(7,19,15,8,3))
+
+# Média harmonica #
+print (3/((1/3.6)+(1/8.9)+(1/10)))
+
+# Média harmonica amortizada #
+
+print ((3/((1/(3.6+4))+(1/(8.9+4))+(1/(10+4))))-4)
