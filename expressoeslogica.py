8844b73b39d80cd5fad3b8e5b9f525c5e0512b3c
Diego Rodolfo Inacio <rodolfo.diego@yahoo.com.br>
2024-02-22 16:55:06 -0300

    Criando arquivo de expressões e operadores de lógica

---
+++ expressoeslogica.py
@@ -0,0 +1,22 @@
+# Relacionais #
+
+print (2 == 2)
+print (2 != 2)
+print (2 < 4)
+print (16 > 8)
+print ( 2 >= 1 )
+print ( 2<= 1)
+
+# Expressão aritimética + Lógica #
+
+print ((2-3)*4 >=0)
+
+# Operadores Lógicos #
+
+print (1>3 or 1+3>2)
+print (13>17 and 17*2>20)
+print (not 13>15 and 15*3 >30)
+print (45 in [45,17,28,65,89])
+print (45 not in [22,11,33,88,45])
+print (4 is not 56)
+print (4.0 is int(4))
